2e39c48aad852d00a5064e536f84bdbea1b00abe
root <root@localhost.localdomain>
2019-11-02 15:35:00 -0400

    convert to PDO

--- modeles.php
+++ modeles.php
@@ -1,4 +1,3 @@
-
 <?php
 /*
 Plugin Name:        Page modèles
@@ -7,101 +6,119 @@ Version:            1
 Author:             Jaël Atam
 Author URI:         portfolio.jaelatam.com
 */
-//Création des post de type modèels
-function ajout_post_modeles()
+
+/**
+ * Class Modele
+ */
+class Modele
 {
-    // On ajoute ici le code des prochaines étapes
-    $labelsModeles = array(
-        'name' => "Modèles",
-        'singular_name' => "Modèle",
-        'add_new' => "Ajouter un modèle",
-        'add_new_item' => "Ajouter un modèle",
-        'edit_item' => "Modifier un modèle",
-        'new_item' => "Nouveau modèle",
-        'all_items' => "Tous les modèles",
-        'view_item' => "Voir le modèle",
-        'search_items' => "Chercher un modèle",
-        'not_found' =>  "Aucun modèle trouvé",
-        'menu_name' => "Mes modèles"
-    );
-    $argModele = array(
-        'labels' => $labelsModeles,
-        'public' => true,
-        'has_archive' => false,
-        'hierarchical' => true,
-        'menu_position' => 20,
-        'menu_icon' => 'dashicons-admin-multisite',
-        'supports' => array('title', 'thumbnail', 'editor', 'revisions', 'page-attributes'),
-        'taxonomies' => array('category', 'post_tag'),
-    );
-    register_post_type('modeles', $argModele);
-}
-add_action( 'init', 'ajout_post_modeles');
+    public function __construct()
+    {
+        /*Ajout de post type modeles*/
+        add_action('init', [$this, 'ajout_post_modeles']);
 
-//Enregistrement du css et js du plugin
-function register(){
+        /*Enregistrement css et js*/
+        add_action('wp_enqueue_scripts', [$this, 'register']);
 
-    wp_register_style( 'modeles', plugins_url( 'css/modeles.css', __FILE__ ) );
-    wp_enqueue_style( 'modeles' );
+        /*Récupérer les modèles en ajax*/
+        add_action('wp_ajax_get_modeles', [$this, 'get_modeles']);
+        add_action('wp_ajax_nopriv_get_modeles', [$this, 'get_modeles']);
 
-    wp_register_script('modeles', plugins_url('js/modeles.js', __FILE__), array('jquery'), '1.0.0', true);
-    wp_enqueue_script('modeles');
+        /*Changer le template modèle par celui du plugin*/
+        add_filter('template_include', [$this,'override_template_modeles']);
 
-    /*Pass Ajax Url to modeles.js*/
-    wp_localize_script('modeles', 'ajaxurl', admin_url('admin-ajax.php') );
+    }
 
-}
-add_action('wp_enqueue_scripts', 'register');
+    /*Fonction ajout de post type modeles*/
+    public function ajout_post_modeles()
+    {
+        $labelsModeles = array(
+            'name' => "Modèles",
+            'singular_name' => "Modèle",
+            'add_new' => "Ajouter un modèle",
+            'add_new_item' => "Ajouter un modèle",
+            'edit_item' => "Modifier un modèle",
+            'new_item' => "Nouveau modèle",
+            'all_items' => "Tous les modèles",
+            'view_item' => "Voir le modèle",
+            'search_items' => "Chercher un modèle",
+            'not_found' => "Aucun modèle trouvé",
+            'menu_name' => "Mes modèles"
+        );
+        $argModele = array(
+            'labels' => $labelsModeles,
+            'public' => true,
+            'has_archive' => false,
+            'hierarchical' => true,
+            'menu_position' => 20,
+            'menu_icon' => 'dashicons-admin-multisite',
+            'supports' => array('title', 'thumbnail', 'editor', 'revisions', 'page-attributes'),
+            'taxonomies' => array('category', 'post_tag'),
+        );
+        register_post_type('modeles', $argModele);
+    }
+    /*Fonction d'enregistrement css et js*/
+    public function register()
+    {
 
-add_action('wp_ajax_get_modeles', 'get_modeles');
-add_action('wp_ajax_nopriv_get_modeles', 'get_modeles');
+        wp_register_style('modeles', plugins_url('css/modeles.css', __FILE__));
+        wp_enqueue_style('modeles');
 
-function get_modeles(){
-    $args = [
-        'post_type' => 'modeles',
-        'post_per_pages' => -1
-    ];
+        if (is_page("modeles")){
+            wp_register_script('modeles', plugins_url('js/modeles.js', __FILE__), array('jquery'), '1.0.0', true);
+            wp_enqueue_script('modeles');
+            /*Pass Ajax Url to modeles.js*/
+            wp_localize_script('modeles', 'ajaxurl', admin_url('admin-ajax.php'));
+        }
 
-    if (isset($_POST["modeles"]) && !empty($_POST["modeles"]) && $_POST["modeles"] != "tous"){
-        $tax_query = ['relation' => 'OR'];
-        $tax_query[] = [
-            'taxonomy' => 'category',
-            'field' => 'slug',
-            'terms' => [$_POST["modeles"]],
-        ];
+    }
+    /*Fonction pour récupérer les modèles en ajax*/
+    public function get_modeles(){
         $args = [
             'post_type' => 'modeles',
-            'post_per_pages' => -1,
-            'tax_query' => $tax_query,
+            'post_per_pages' => -1
         ];
-    }
 
-    $ajax_query_modeles = new WP_Query( $args );
-
-    echo "<div class='flex'>";
-    if ($ajax_query_modeles->have_posts()){
-        while ($ajax_query_modeles->have_posts() ){
-            $ajax_query_modeles->the_post();
-            echo "<div>";
-            the_post_thumbnail( 'thumbnail' );
-            echo "</div>";
+        if (isset($_POST["modeles"]) && !empty($_POST["modeles"]) && $_POST["modeles"] != "tous"){
+            $tax_query = ['relation' => 'OR'];
+            $tax_query[] = [
+                'taxonomy' => 'category',
+                'field' => 'slug',
+                'terms' => [$_POST["modeles"]],
+            ];
+            $args = [
+                'post_type' => 'modeles',
+                'post_per_pages' => -1,
+                'tax_query' => $tax_query,
+            ];
         }
+
+        $ajax_query_modeles = new WP_Query( $args );
+
+        echo "<div class='flex'>";
+        if ($ajax_query_modeles->have_posts()){
+            while ($ajax_query_modeles->have_posts() ){
+                $ajax_query_modeles->the_post();
+                echo "<div>";
+                the_post_thumbnail( 'thumbnail' );
+                echo "</div>";
+            }
+        }
+        else{
+            echo "Aucun ne résultat ne correspond à la recherche";
+        }
+        echo "</div>";
+        die();
     }
-    else{
-        echo "Aucun ne résultat ne correspond à la recherche";
+
+    /*Fonction pour changer le template modèle par celui du plugin*/
+    public function override_template_modeles($modeles_template)
+    {
+        if (is_page('modeles')) {
+            $modeles_template = dirname(__FILE__) . '/templates/page-modeles.php';
+        }
+        return $modeles_template;
     }
-    echo "</div>";
-    die();
 }
 
-//Template personnalisé pour le plugin
-function override_template_modeles( $modeles_template )
-{
-    if (is_page( 'modeles' )) {
-        $modeles_template = dirname( __FILE__ ) . '/templates/page-modeles.php';
-    }
-    return $modeles_template;
-}
-add_filter('template_include', 'override_template_modeles');
-
-
+new Modele();
--- templates/page-modeles.php
+++ templates/page-modeles.php
@@ -22,21 +22,21 @@ if ( ! defined( 'ABSPATH' ) ) {
             </select>
             <select name='chambres' id='chambres'>
                 <option value='tous'>Tous</option>
-                <option value='1'>1</option>
-                <option value='2'>2</option>
-                <option value='3+'>3 et +</option>
+                <option value='1-chambres'>1 Chambre</option>
+                <option value='2-chambres'>2 Chambres</option>
+                <option value='3-chambres'>3 Chambres et +</option>
             </select>
             <select name='sbd' id='sdb'>
                 <option value='tous'>Tous</option>
-                <option value='1'>1</option>
-                <option value='2'>2</option>
-                <option value='3+'>3 et +</option>
+                <option value='1-sdb'>1 Salle de bain</option>
+                <option value='2-sdb'>2 Salles de bain</option>
+                <option value='3-sdb'>3 Salles de bain et +</option>
             </select>
             <select name='superficie' id='superficie'>
                 <option value='tous'>Tous</option>
                 <option value='1000-1400'>1000 à 1400p2</option>
                 <option value='1401-1800'>1401 à 1800p2</option>
-                <option value='1801+'>1801p2 et +</option>
+                <option value='1801p2'>1801p2 et +</option>
             </select>
             <button class='btn' type='submit'>Rechercher</button>
         </form>
